4e3d1900ec173db401331ad8691c86f929bad464
anneewhere <114482462+anneewhere@users.noreply.github.com>
2023-01-21 09:41:06 +0800

    permissions
    homework completed

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
+from .permissions import IsOwnerOrReadOnly
 
 #linking the serializer and the model
 from .models import Project, Pledge
@@ -27,9 +28,16 @@ class ProjectList(APIView):
 
 class ProjectDetail(APIView): #shows us specific details of the project
 
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+        ] #to get the projectListAPI, must pass through this permission
+
     def get_object(self,pk): #this is defined in bottom def
         try:
-            return Project.objects.get(pk=pk) #primary key equals the value it was given. if get_object(self,monkey), then on this line it is (pk=monkey)
+            project = Project.objects.get(pk=pk) #primary key equals the value it was given. if get_object(self,monkey), then on this line it is (pk=monkey)
+            self.check_object_permissions(self.request, project)
+            return project        
         except Project.DoesNotExist:
             raise Http404 #when we raise an error, django knows how to handle it.
 
@@ -37,6 +45,17 @@ class ProjectDetail(APIView): #shows us specific details of the project
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
 
 class PledgeList(generics.ListCreateAPIView): #lists and creates the view. using 'generic' helps us create a form instead of showing it as a JSON. 
     queryset = Pledge.objects.all()
