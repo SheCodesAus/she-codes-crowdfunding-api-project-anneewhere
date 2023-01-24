06ece3564254013e31b9f6a5ae8c9479bd33f25b
anneewhere <114482462+anneewhere@users.noreply.github.com>
2023-01-24 19:45:41 +0800

    24 january work

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    username = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(_("email address"), unique=True)
+    profile_picture = models.URLField(blank=True, null=True)
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.username
